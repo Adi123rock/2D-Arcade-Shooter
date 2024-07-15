15fd498ff42d454e690052aa6ef468965a5bb75a
Hruday2111 <hrudayamrit21@gmail.com>
2024-07-15 18:38:42 +0530

    waves

--- 2D Arcade Shooter main/Assets/Scripts/Bulletcollision.cs
+++ 2D Arcade Shooter main/Assets/Scripts/Bulletcollision.cs
@@ -1,13 +1,18 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class bulletcollision : MonoBehaviour
 {
     public int health=100;
+
+    public Animator headinganimator,villananimator ;
+    static int EnemyKilled=0;
     public static int damage=50;
     //public Transform  obst;
     public GameObject explosionEffect,obstacle;
+    bool w1don,w2don,w3don,w4don = false;
 
     void OnTriggerEnter2D(Collider2D collisionInfo)
     {
@@ -36,9 +41,41 @@ public class bulletcollision : MonoBehaviour
     void Explode()
     {
         // Instantiate(explosionEffect,transform.position,transform.rotation);
-        Debug.Log("off");
+        //Debug.Log("off");
         Destroy(obstacle);
+        EnemyKilled++;
+        //Debug.Log(EnemyKilled);
+        // if(animator != null && animator2 != null){
+        //     Debug.Log("yep");
+        // animator.SetFloat("EnemyKilled",EnemyKilled);
+        // animator2.SetFloat("EnemyKilled",EnemyKilled);}
         //explosionEffect.SetActive(false);
+    }
+
+    void Update() {
+        
+        if(headinganimator != null && villananimator != null){
+           // Debug.Log(EnemyKilled);
+        headinganimator.SetInteger("EnemyKilled",EnemyKilled);
+        villananimator.SetInteger("EnemyKilled",EnemyKilled);}
+
+
+        if((EnemyKilled ==1 && w1don == false)){
+            FindObjectOfType<NewBehaviourScript>().fastmoving();
+            w1don = true ;
+        }
+        if((EnemyKilled >2 && w2don == false)){
+            FindObjectOfType<NewBehaviourScript>().fastmoving();
+            w2don = true ;
+        }
+        if((EnemyKilled >5 && w3don == false)){
+            FindObjectOfType<NewBehaviourScript>().fastmoving();
+            w3don = true ;
+        }
+        if((EnemyKilled >9 && w4don == false)){
+            FindObjectOfType<NewBehaviourScript>().fastmoving();
+            w4don = true ;
+        }
     }
     void Destroyexpl()
     {
--- 2D Arcade Shooter main/Assets/Scripts/Obstaclefiring.cs
+++ 2D Arcade Shooter main/Assets/Scripts/Obstaclefiring.cs
@@ -9,39 +9,46 @@ public class Obstaclefiring : MonoBehaviour
     int i=0;
     public Transform wrt_camera,enb;
     public float enbforce;
-    public Rigidbody2D enblt;
+    public GameObject enblt;
+
+
     // Update is called once per frame
     void Start()
     {
-        if(transform.position.y<(wrt_camera.position.y+9))
-        {
+        //if(transform.position.y<(wrt_camera.position.y+9))
+        //{
             // enb.position=transform.position;
             // enblt.AddForce(new Vector2(0,-enbforce*Time.deltaTime));
             //i++;
-        }
+        //}
         
     }
 
     void FixedUpdate()
     {
         //-4 since camera position 4 ahead of player
-        if(transform.position.y>wrt_camera.position.y-4 && enb.position.y<(transform.position.y-9) && transform.position.y<(wrt_camera.position.y+9-4))
-        {
-            enblt.AddForce(new Vector2(0,enbforce*Time.deltaTime));
-            //enblt.AddForce(0,enbforce*Time.deltaTime,0,ForceMode.VelocityChange);
-            enb.position=transform.position;
-            //enblt.AddForce(0,-enbforce*Time.deltaTime,0,ForceMode.VelocityChange);
-            enblt.AddForce(new Vector2(0,-enbforce*Time.deltaTime));
-        }
+        // if(transform.position.y>wrt_camera.position.y-4 && enb.position.y<(transform.position.y-9) && transform.position.y<(wrt_camera.position.y+9-4))
+        // {
+        //     enblt.AddForce(new Vector2(0,enbforce*Time.deltaTime));
+        //     //enblt.AddForce(0,enbforce*Time.deltaTime,0,ForceMode.VelocityChange);
+        //     enb.position=transform.position;
+        //     //enblt.AddForce(0,-enbforce*Time.deltaTime,0,ForceMode.VelocityChange);
+        //     enblt.AddForce(new Vector2(0,-enbforce*Time.deltaTime));
+        // }
         
-        else if(i==0 && transform.position.y>wrt_camera.position.y-4 && transform.position.y<(wrt_camera.position.y+9-4))
-        {
-            i++;
+        // else if(i==0 && transform.position.y>wrt_camera.position.y-4 && transform.position.y<(wrt_camera.position.y+9-4))
+        // {
+        //     i++;
             
-            enb.position=transform.position;
-            enblt.AddForce(new Vector2(0,-enbforce*Time.deltaTime));
-            //enblt.AddForce(0,-enbforce*Time.deltaTime,0,ForceMode.VelocityChange);
-        }
+        //     enb.position=transform.position;
+        //     enblt.AddForce(new Vector2(0,-enbforce*Time.deltaTime));
+        //     //enblt.AddForce(0,-enbforce*Time.deltaTime,0,ForceMode.VelocityChange);
+        // }
+        // remainingtime = remainingtime - Time.deltaTime ;
+        // if(remainingtime<= 0){
+        //     firedblt = Instantiate(enblt , firepoint.position , firepoint.rotation);
+        //     remainingtime = 3f ;
+        // }
     }
     //transform.position.y>player.position.y && enb.position.y<player.position.y
 
